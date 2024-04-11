8923e188177dfbfbeb4bbe585d08e2f48f13db4d
Hadassa Jhulia <163539809+HadassaJhulia@users.noreply.github.com>
2024-04-11 11:59:28 -0300

    Add files via upload

---
+++ Aula5/aula5.php
@@ -0,0 +1,42 @@
+<?php
+
+    echo "<br>";
+    echo "###############################";
+    echo "<br>";
+    echo (pi());
+    echo "<br>";
+    echo "###############################";
+    echo "<br>";
+    echo(min(0, 150, 30, 20, -8, -200));
+    echo "<br>";
+    echo "###############################";
+    echo "<br>";
+    echo(max(0, 150, 30, 20, -8, -200));
+    echo "<br>";
+    echo "###############################";
+    echo "<br>";
+    echo(sqrt(25)); //raiz quadrada
+    echo "<br>";
+    echo "###############################";
+    echo "<br>";
+    echo(rand());
+    echo "<br>";
+    echo "###############################";
+    echo "<br>";
+    echo($letra = chr(64+rand(0,25)));
+    echo "<br>";
+    echo "###############################";
+    echo "<br>";
+    echo(sin(deg2rad(45))); //seno
+    echo "<br>";
+    echo "###############################";
+    echo "<br>";
+    echo(cos(deg2rad(45))); //cosseno
+    echo "<br>";
+    echo "###############################";
+    echo "<br>";
+    echo(tan(deg2rad(45))); //tangente
+    echo "<br>";
+    echo "###############################";
+    echo "<br>";
+?>
