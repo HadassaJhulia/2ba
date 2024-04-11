7dfd13ae100ce5e04bf8cc97334e5b39889d9c77
Hadassa Jhulia <163539809+HadassaJhulia@users.noreply.github.com>
2024-04-11 11:58:31 -0300

    Add files via upload

---
+++ Aula2/aula2.php
@@ -0,0 +1,5 @@
+<?php
+    echo("<h1>hello word!</h1><br>");
+    echo("bartô<br>");
+    echo("bartôzinho<br>");
+?>
---
+++ Aula2/aula2b.php
@@ -0,0 +1,16 @@
+<?php
+    $nome = "Hadassa Jhulia";
+    $sobrenome = "Lima";
+    $idade = "15 anos";
+    $escola = "Etec Bartolomeu Bueno da Silva - Anhanguera";
+
+    echo ("<strong>Nome:</strong> " .$nome. "<br>");
+    echo ("<strong>Sobrenome:</strong> " .$sobrenome. "<br>");
+
+    echo ("<strong>Nome copleto:</strong> ". $nome ." ". $sobrenome ."<br>");
+    echo ("<strong>Idade:</strong> ". $idade ."<br>");
+    echo ("<strong>Instituição:</strong> ". $escola ."<br>");
+
+    echo("<h2>PHP is fun!</h2>");
+
+?>
